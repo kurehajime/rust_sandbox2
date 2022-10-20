2cc610897e2f6374aa818b86af06536a4c3cc9b7
kurehajime <xiidec@gmail.com>
2022-10-20 22:46:56 +0900

    BigToString

--- genetrait/src/main.rs
+++ genetrait/src/main.rs
@@ -1,23 +1,16 @@
-fn largest<T: PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+use std::fmt::Display;
 
-    for item in list {
-        if item > largest {
-            largest = &item;
-        }
+trait BigToString {
+    fn big_to_string(&self) -> String;
+}
+
+impl<T: Display> BigToString for T {
+    fn big_to_string(&self) -> String {
+        format!("{}", self).to_ascii_uppercase()
     }
-
-    largest
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    let s = "hello world";
+    println!("{}", s.big_to_string());
 }
